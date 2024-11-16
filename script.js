3348628fa164e5754aa362e7cfd12ffb930e7297
SyedMohammed <syedmohdah@gmail.com>
2024-11-16 20:12:48 +0530

    Made Changes to the Entire UI

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function addelement(){
-    let container=document.querySelector("#container");
+    let taskContainer=document.querySelector("#newTaskcontainer");
     let input_text=document.querySelector("#input_field");
     let task=input_text.value;
     
@@ -7,11 +7,79 @@ function addelement(){
     if(task.trim()!==""){
         let new_task=document.createElement("ul");
         new_task.innerText=task;
-        // for (const element of new_task) {
-    //     console.log(element)
         
-    // }
-        container.appendChild(new_task);
+        new_task.style.border="2px"
+
+
+    let wrapper=document.createElement("div");
+    wrapper.style.display="block";
+    wrapper.style.position="relative";
+
+    wrapper.style.border="1px solid black"
+    wrapper.style.borderRadius="5px"
+    wrapper.style.marginTop="10px"
+    wrapper.style.width="300px"
+    wrapper.style.marginLeft="10px"
+    wrapper.style.marginRight="10px"
+    wrapper.style.transition="transform 0.3s linear, background-color 0.3s linear"
+    wrapper.style.transform="scale(1)";
+    wrapper.style.cursor="pointer";
+
+    document.body.appendChild(wrapper);
+    wrapper.addEventListener("mouseover",()=>{
+        wrapper.style.transform="scale(1.1)"
+    })
+
+    wrapper.addEventListener("mouseout",()=>{
+        wrapper.style.transform="scale(1)"
+    })
+
+    let target_div=document.querySelector("#doneTaskcontainer");
+    // target_div.style.height="100px";
+    // target_div.style.width="100px";
+
+    let finishButton=document.createElement("button");
+    finishButton.innerHTML="&#10004;"
+    finishButton.style.backgroundColor="blue";
+    finishButton.style.color="white";
+    finishButton.style.position="absolute";
+    finishButton.style.top="14px";
+    finishButton.style.left="10px"
+    finishButton.style.cursor="pointer";
+    finishButton.style.border="none";
+    finishButton.style.borderRadius="50%";
+
+
+    finishButton.addEventListener("click",()=>{
+        // let clonedWrapper=wrapper.cloneNode(true);
+        let buttons=wrapper.querySelectorAll("button");
+        buttons.forEach((element) => element.remove());
+        target_div.appendChild(wrapper)
+    })
+    
+
+    let remove_Btn=document.createElement("button");
+    remove_Btn.innerText="X"
+    remove_Btn.style.color="white";
+    remove_Btn.style.backgroundColor="red";
+    remove_Btn.style.position="absolute";
+    remove_Btn.style.left="130px";
+    remove_Btn.style.top="14px"
+    // remove_Btn.style.right="30px"
+    remove_Btn.style.cursor="pointer";
+    remove_Btn.style.border="none";
+    remove_Btn.style.borderRadius="50%";
+    remove_Btn.style.width = "20px"; 
+    remove_Btn.style.height = "20px";
+    remove_Btn.addEventListener("click",()=>{
+        taskContainer.removeChild(wrapper);  
+    })
+        wrapper.appendChild(finishButton);
+        wrapper.appendChild(remove_Btn);
+        wrapper.appendChild(new_task);
+        taskContainer.appendChild(wrapper);
+    }else{
+        alert("Please Enter Something")
     }
     
     input_text.value="";
@@ -20,28 +88,28 @@ function addelement(){
 
 
 function removeelement(){
-    let maincotainer=document.querySelector("#container");
+    let maincotainer=document.querySelector("#newTaskcontainer");
     let lastChild=maincotainer.lastChild;
     if(lastChild){
         maincotainer.removeChild(lastChild)
     }
 }
 
-function removeall(){
-    let maincotainer=document.querySelector("#container");
-    maincotainer.innerText="";
-}
+// function removeall(){
+//     let maincotainer=document.querySelector("#newTaskcontainer");
+//     maincotainer.innerText="";
+// }
 
 
-const saveall=()=>{
-let link=document.createElement("a");
-const container=document.querySelector("#container");
+// const saveall=()=>{
+// let link=document.createElement("a");
+// const container=document.querySelector("#newTaskcontainer");
 
-let task=Array.from(container.children).map(task=>task.innerText).join("\n");
-const file=new Blob([task],{type:"text/plain"});
-link.href=URL.createObjectURL(file);
-link.download="task.txt";
-link.click();
+// let task=Array.from(container.children).map(task=>task.innerText).join("\n");
+// const file=new Blob([task],{type:"text/plain"});
+// link.href=URL.createObjectURL(file);
+// link.download="task.txt";
+// link.click();
 // URL.revokeObjectURL(link.href);
 
-}
+// }
